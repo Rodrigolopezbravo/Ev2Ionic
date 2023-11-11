b4ece9b8b17e5401131dd8bb9af8ef3394dc5f17
Rodrigolopezbravo <rodrigolopezbrabo6@gmail.com>
2023-11-11 00:21:38 -0300

    creacion de login y registro

--- EV2ionic/src/app/app-routing.module.ts
+++ EV2ionic/src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
 ];
 
 @NgModule({
---
+++ EV2ionic/src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formularioLogin: FormGroup;
+
+  constructor(public fb:FormBuilder) { 
+
+    this.formularioLogin = this.fb.group({
+      'usuario': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+    });
+    
+  }
+
+  ngOnInit() {
+  }
+
+  
+  
+}
