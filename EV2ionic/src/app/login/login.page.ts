ce32a7a71173b33d0bc874f751f0d6e0a93c12a4
Rodrigolopezbravo <rodrigolopezbrabo6@gmail.com>
2023-11-11 01:45:57 -0300

    Utilizando capacitor para registrar y ingresar usuarios

--- EV2ionic/src/app/login/login.page.ts
+++ EV2ionic/src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
-
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-login',
@@ -11,18 +12,45 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb:FormBuilder) { 
-
+  constructor(public fb: FormBuilder, public alertController: AlertController) { 
     this.formularioLogin = this.fb.group({
-      'usuario': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required),
+      'usuario': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
     });
-    
   }
 
   ngOnInit() {
   }
 
-  
-  
+  async ingresar() {
+    var f = this.formularioLogin.value;
+
+    // Construir la clave utilizando el nombre de usuario
+    const clave = `usuario_${f.usuario}`;
+
+    // Obtener el usuario de las preferencias de Capacitor
+    const usuarioString = await Preferences.get({ key: clave });
+
+    if (usuarioString && usuarioString.value) {
+      const usuario = JSON.parse(usuarioString.value);
+
+      if (usuario.password === f.password) {
+        console.log('Ingresado correctamente');
+      } else {
+        const alert = await this.alertController.create({
+          header: 'Datos incorrectos',
+          message: 'Los datos ingresados no son correctos',
+          buttons: ['Aceptar']
+        });
+        await alert.present();
+      }
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Usuario no encontrado',
+        message: 'No se encontró un usuario con el nombre proporcionado',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+  }
 }
--- EV2ionic/src/app/registro/registro.page.ts
+++ EV2ionic/src/app/registro/registro.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-registro',
@@ -10,19 +12,33 @@ export class RegistroPage implements OnInit {
 
   formularioRegistro: FormGroup;
 
-  constructor(public fb: FormBuilder) { 
-
+  constructor(public fb: FormBuilder, public alertController: AlertController) { 
     this.formularioRegistro = this.fb.group({
-      'nombre': new FormControl("",Validators.required),
-      'apellido': new FormControl("",Validators.required),
-      'rut': new FormControl("",Validators.required),
-      'usuario': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required)
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'rut': new FormControl("", Validators.required),
+      'usuario': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
     });
-
   }
 
   ngOnInit() {
   }
 
+  async guardar() {
+    var f = this.formularioRegistro.value;
+
+    // Utilizar el nombre de usuario como parte de la clave
+    const clave = `usuario_${f.usuario}`;
+
+    // Almacenar usuario en las preferencias de Capacitor
+    await Preferences.set({ key: clave, value: JSON.stringify(f) });
+
+    const alert = await this.alertController.create({
+      header: 'Registro exitoso',
+      message: 'El usuario se ha registrado correctamente',
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
 }
